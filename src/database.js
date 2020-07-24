91114bf2311e21452288d8d2b2dd5adf212818ae
Mariana Mattos <marianasmmattos@gmail.com>
2020-07-24 18:38:02 -0300

    commit

---
+++ src/database.js
@@ -0,0 +1,37 @@
+const sqlite = require("sqlite3").verbose()
+
+const db = new sqlite.Database("./src/database.db")
+
+db.serialize(() => {
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            message TEXT
+        );
+    `)
+   
+   const query = `
+        INSERT INTO places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT
+            message,
+        ) VALUES (
+            ?
+        );
+    `
+ 
+    const values = [
+        'mensagem'
+    ]
+ 
+    function afterInsertData (err){
+        if (err) {
+            return console.log(err)
+         }
+    }
+ 
+    db.run(query, values, afterInsertData);
+ 
+})
+
+db.serialize()
