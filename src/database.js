ea5251533845bc37c8879e6d9c59a1e7dd1ac018
Mariana Mattos <marianasmmattos@gmail.com>
2020-07-25 13:37:14 -0300

    commit

--- src/database.js
+++ src/database.js
@@ -1,36 +1,46 @@
 const sqlite = require("sqlite3").verbose()
-
 const db = new sqlite.Database("./src/database.db")
 
-db.serialize(() => {
+module.exports = db;
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS places (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            message TEXT
-        );
-    `)
+// db.serialize(() => {
+
+//    db.run(`
+//        CREATE TABLE IF NOT EXISTS places (
+//            id INTEGER PRIMARY KEY AUTOINCREMENT,
+//            message TEXT
+//        );
+//    `)
    
-   const query = `
-        INSERT INTO places (
-            message
-        ) VALUES (
-            ?
-        );
-    `
+//   const query = `
+//        INSERT INTO places (
+//            message
+//        ) VALUES (
+//            ?
+//        );
+//    `
  
-    const values = [
-        "mensagem"
-    ]
+//    const values = [
+//        "mensagem"
+//    ]
  
-    function afterInsertData (err){
-        if (err) {
-            return console.log(err)
-         }
+//    function afterInsertData (err){
+//        if (err) {
+//            return console.log(err)
+//         }
 
-   }
+//   }
  
-    db.run(query, values, afterInsertData);
-})
+//    db.run(query, values, afterInsertData);
+//})
 
-db.serialize()
+//db.run(query, values, afterInsertData)
+
+// db.run(`DELETE FROM places WHERE id = ?`, [4], function(err){
+//        if (err) {
+//            return console.log(err)
+//        }
+//       console.log("Registro deletado")  
+//  })
+
+// db.serialize()
